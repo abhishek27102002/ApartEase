5561636973a74b9cf70483c44190fb806dcde21a
abhishek27102002 <gupta92345@gmail.com>
2024-10-11 02:49:12 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017'; // Change this if using a different host
+// const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://utsavraj626:cW6I6il4sRop4cRj@cluster0.trrzn.mongodb.net/xenon?retryWrites=true&w=majority&appName=Cluster0';
 const dbName = 'vacationRentals';
 let db;
 
